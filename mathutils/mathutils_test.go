ba148500d1e76cfbf29eb9222ddf32934e1c0d5c
Abdelrahman Taman <taman9333@gmail.com>
2024-12-04 13:38:14 +0200

    Add new flaky test

--- mathutils/mathutils_test.go
+++ mathutils/mathutils_test.go
@@ -1,6 +1,9 @@
 package mathutils
 
-import "testing"
+import (
+	"math/rand/v2"
+	"testing"
+)
 
 func TestAdd(t *testing.T) {
 	result := Add(2, 3)
@@ -13,5 +16,11 @@ func TestMultiply(t *testing.T) {
 	result := Multiply(2, 3)
 	if result != 6 {
 		t.Errorf("Expected 6, got %d", result)
+	}
+}
+
+func Test_AddFlaky(t *testing.T) {
+	if rand.Float32() < 0.7 {
+		t.Fatal("Flaky test failed!")
 	}
 }
